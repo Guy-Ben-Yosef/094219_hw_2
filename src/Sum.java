120461a27cf98a01eddf420d08b73bca8b07f1f5
theguyben <guyben@campus.technion.ac.il>
2023-06-03 15:09:29 +0300

    `Sum` class implementation

--- src/Sum.java
+++ src/Sum.java
@@ -1,3 +1,40 @@
 /* This class represents a function of type sum*/
-public class Sum {
+public class Sum extends Function {
+    Function f1;
+    Function f2;
+
+    /**
+     * Constructor for Sum
+     * @param f1 a Function input
+     * @param f2 a Function input
+     */
+    public Sum(Function f1, Function f2) {
+        this.f1 = f1;
+        this.f2 = f2;
+    }
+
+    /**
+     * @param x The coordinate at which to evaluate the function
+     * @return the value of the sum of the two functions at x
+     */
+    @Override
+    public double valueAt(double x) {
+        return f1.valueAt(x) + f2.valueAt(x);
+    }
+
+    /**
+     * @return A string representation of the sum of the two functions
+     */
+    @Override
+    public String toString() {
+        return "(" + f1.toString() + "+" + f2.toString() + ")";
+    }
+
+    /**
+     * @return the derivative of the sum of the two functions
+     */
+    @Override
+    public Sum derivative() {
+        return new Sum(f1.derivative(), f2.derivative());
+    }
 }
