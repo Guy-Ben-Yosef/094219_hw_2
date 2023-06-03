889546b76d89b46c026e2f8ea0f663765bec3a19
theguyben <guyben@campus.technion.ac.il>
2023-06-03 19:22:17 +0300

    `Quotient` class implementation

---
+++ src/Quotient.java
@@ -0,0 +1,42 @@
+/* This class represents rational function */
+public class Quotient extends Function {
+    Function numerator;
+    Function denominator;
+
+    /**
+     * Constructor for Quotient
+     * @param numerator The numerator of the rational function
+     * @param denominator The denominator of the rational function
+     */
+    public Quotient(Function numerator, Function denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    /**
+     * @param x The coordinate at which to evaluate the function
+     * @return the value of the rational function at x
+     */
+    @Override
+    public double valueAt(double x) {
+        return numerator.valueAt(x) / denominator.valueAt(x);
+    }
+
+    /**
+     * @return The string representation of the rational function
+     */
+    @Override
+    public String toString() {
+        return "(" + numerator.toString() + ")/(" + denominator.toString() + ")";
+    }
+
+    /**
+     * @return The derivative of the rational function
+     */
+    @Override
+    public Quotient derivative() {
+        Function fPrimeTimesG = new Product(numerator.derivative(), denominator);  // f' * g
+        Function gPrimeTimesF = new Product(numerator, denominator.derivative());  // g' * f
+        return new Quotient(Difference(fPrimeTimesG, gPrimeTimesF), Power(denominator, 2));  // (f' * g - g' * f) / g^2
+    }
+}
