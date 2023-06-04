899cf9d9e0b5cec35ffb37d3d34c5e8218fc0c84
theguyben <guyben@campus.technion.ac.il>
2023-06-04 07:45:51 +0300

    BugFix

--- src/Constant.java
+++ src/Constant.java
@@ -10,16 +10,24 @@ public class Constant extends Function{
         this.f = number;
     }
 
+    /**
+     * @param point the coordinate at which to evaluate the function
+     * @return since it is constant function, this function always returns the constant
+     */
     @Override
-    public double valueAt(double point) {
-        return 0;
-    }
+    public double valueAt(double point) { return f; }
 
+    /**
+     * @return a string representation of the constant
+     */
     @Override
     public String toString() {
         return String.valueOf(f);
-    }
+    }  // TODO: should it return "7.0" or "7"?
 
+    /**
+     * @return the derivative of the constant which is always zero
+     */
     @Override
     public Function derivative() {
         return new Constant(0);
